87ab145cf33378c39ede5c304791714bf118d123
Abhinav Awasthi <56489028+abhinavawasthi1729@users.noreply.github.com>
2025-07-20 23:13:55 +0530

    Update searchSlice.js

--- src/redux/searchSlice.js
+++ src/redux/searchSlice.js
@@ -1,44 +1,68 @@
+// Importing createSlice utility from Redux Toolkit for easier slice creation
 import { createSlice } from "@reduxjs/toolkit";
 
+// Creating a slice to manage search-related state in the application
 export const searchSlice = createSlice({
-  name: "search",
+  name: "search", // Name of the slice (used as key in the store)
   initialState: {
-    user: null,
-    platforms: [],
-    heatmaps: [],
-    isFetching: false,
-    error: false,
+    user: null,        // Stores user data from search results
+    platforms: [],     // Stores coding platforms data related to the searched user
+    heatmaps: [],      // Stores heatmap data for searched user/platforms
+    isFetching: false, // Indicates whether a search or fetch operation is in progress
+    error: false,      // Indicates whether the last operation resulted in an error
   },
   reducers: {
-    // Reducer for starting a search operation
+    /**
+     * Triggered when a search operation begins
+     * Sets isFetching flag to true for loading states
+     */
     searchStart: (state) => {
       state.isFetching = true;
     },
-    // Reducer for successful search operation
+
+    /**
+     * Triggered when search is successful
+     * Updates user data and resets loading and error flags
+     */
     searchSuccess: (state, action) => {
       state.isFetching = false;
       state.error = false;
       state.user = action.payload.user;
     },
-    // Reducer for successful fetch of coding platforms data
+
+    /**
+     * Triggered when coding platforms data is successfully fetched
+     * Updates platforms array and resets loading/error flags
+     */
     platformsFetchSuccess: (state, action) => {
       state.isFetching = false;
       state.error = false;
       state.platforms = action.payload.platforms;
     },
-    // Reducer for successful fetch of heatmaps data
+
+    /**
+     * Triggered when heatmap data is successfully fetched
+     * Updates heatmaps array (note: expects heatmaps in `action.payload.platforms`)
+     */
     heatmapsFetchSuccess: (state, action) => {
       state.isFetching = false;
       state.error = false;
       state.heatmaps = action.payload.platforms;
     },
-     // Reducer for search failure
+
+    /**
+     * Triggered when any search-related operation fails
+     * Sets error flag and resets loading flag
+     */
     searchFailure: (state) => {
       state.isFetching = false;
       state.error = true;
     },
-        // Reducer for clearing search-related state
 
+    /**
+     * Clears the entire search-related state
+     * Useful when switching users or clearing the form
+     */
     clearsearch: (state) => {
       state.isFetching = false;
       state.error = false;
@@ -49,13 +73,16 @@ export const searchSlice = createSlice({
   },
 });
 
+// Exporting actions to be used in components or thunks
 export const {
   searchStart,
   searchFailure,
   searchSuccess,
-  platformSetSuccess,
+  platformSetSuccess, // NOTE: This action doesn't exist in the reducers above
   platformsFetchSuccess,
   heatmapsFetchSuccess,
   clearsearch,
 } = searchSlice.actions;
+
+// Exporting the reducer function to be added to the Redux store
 export default searchSlice.reducer;
